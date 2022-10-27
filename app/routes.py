1b3dd88bc8bf86da7a1a7410bae90a3c7ae9a074
fataneh <fkarandish1@gmail.com>
2022-10-27 14:54:30 -0700

    create a model, create planet table, add function create_planet

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,26 @@
-from flask import Blueprint, jsonify
 
-class Planet:
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify,request
 
-    def __init__(self, id, name, description, temp):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.temp = temp
+# class Planet:
 
-planets = [Planet(1, "Earth", "Our world", 50), 
-           Planet(2, "Mars", "hostile to life",-81), 
-           Planet(3, "Venus", "Earth's sister", 900)]
+#     def __init__(self, id, name, description, temp):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.temp = temp
+
+# planets = [Planet(1, "Earth", "Our world", 50), 
+#            Planet(2, "Mars", "hostile to life",-81), 
+#            Planet(3, "Venus", "Earth's sister", 900)]
 
 
 planet_bp = Blueprint("planet_bp", __name__, url_prefix="/planets")
 
 @planet_bp.route("", methods=["GET"])
 def get_all_planets():
+    planets = Planet.query.all()
     planet_response = []
     for planet in planets:
         planet_dict = {
@@ -27,3 +31,13 @@ def get_all_planets():
         }
         planet_response.append(planet_dict)
     return jsonify(planet_response), 200
+
+@planet_bp.route("", methods= ["POST"])
+def create_planet():
+    request_body= request.get_json()
+    new_planet = Planet(name = request_body["name"], description =request_body["description"],
+    temp = request_body["temp"])
+
+    db.session.add(new_planet)
+    db.session.commit()
+    return {"id": new_planet.id, "name":new_planet.name, "description": new_planet.description, "temp":new_planet.temp},201
